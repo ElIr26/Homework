0ba6d6be358358566d931f115df874555836323b
Ellr26 <eaborovkova@mail.ru>
2026-04-03 11:36:07 +0500

    Удален старый Main.java

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
 public class Homework1 {
     public static void main(String[] args) {
 
@@ -14,8 +18,8 @@ public class Homework1 {
         compareNumbers();
 
         // ЗАДАНИЕ 5
-        System.out.println(isSumBetween10And20(5, 7));
-        System.out.println(isSumBetween10And20(1, 2));
+        out.println(isSumBetween10And20(5, 7));
+        out.println(isSumBetween10And20(1, 2));
 
         // ЗАДАНИЕ 6
         checkPositiveOrNegative(5);
@@ -23,107 +27,87 @@ public class Homework1 {
         checkPositiveOrNegative(0);
 
         // ЗАДАНИЕ 7
-        System.out.println(isNegative(5));
-        System.out.println(isNegative(-3));
+        out.println(isNegative(5));
+        out.println(isNegative(-3));
 
         // ЗАДАНИЕ 8
-        printStringMultipleTimes("Hello, QA!", 3);
+        printStringMultipleTimes("Привет, Влад!", 3);
 
         // ЗАДАНИЕ 9
-        System.out.println(isLeapYear(2020));
-        System.out.println(isLeapYear(1900));
+        out.println(isLeapYear(2020));
+        out.println(isLeapYear(1900));
 
         // ЗАДАНИЕ 10
         int[] binaryArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.print("Исходный: ");
-        for (int i = 0; i < binaryArray.length; i++) {
-            System.out.print(binaryArray[i] + " ");
+        out.print("Исходный: ");
+        for (int k : binaryArray) {
+            out.print(k + " ");
         }
-        System.out.println();
+        out.println();
         invertBinaryArray(binaryArray);
-        System.out.print("Результат: ");
-        for (int i = 0; i < binaryArray.length; i++) {
-            System.out.print(binaryArray[i] + " ");
+        out.print("Результат: ");
+        for (int k : binaryArray) {
+            out.print(k + " ");
         }
-        System.out.println();
+        out.println();
 
         // ЗАДАНИЕ 11
         int[] hundredArray = createArray1To100();
-        System.out.print("Первые 10 элементов: ");
+        out.print("Первые 10 элементов: ");
         for (int i = 0; i < 10; i++) {
-            System.out.print(hundredArray[i] + " ");
+            out.print(hundredArray[i] + " ");
         }
-        System.out.println();
+        out.println();
 
         // ЗАДАНИЕ 12
         int[] numbers = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.print("До: ");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        out.print("До: ");
+        for (int number : numbers) {
+            out.print(number + " ");
         }
-        System.out.println();
+        out.println();
         multiplyLessThan6(numbers);
-        System.out.print("После: ");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        out.print("После: ");
+        for (int number : numbers) {
+            out.print(number + " ");
         }
-        System.out.println();
+        out.println();
 
         // ЗАДАНИЕ 13
         int[][] matrix = createDiagonalMatrix(5);
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                out.print(anInt + " ");
             }
-            System.out.println();
+            out.println();
         }
 
         // ЗАДАНИЕ 14
         int[] filled = fillArray(7, 42);
-        for (int i = 0; i < filled.length; i++) {
-            System.out.print(filled[i] + " ");
+        for (int j : filled) {
+            out.print(j + " ");
         }
-        System.out.println();
+        out.println();
     }
 
     // ЗАДАНИЕ 1
     public static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple");
+        out.println("Orange\nBanana\nApple");
     }
 
     // ЗАДАНИЕ 2
     public static void checkSumSign() {
-        int a = 10;
-        int b = -5;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
+        out.println("Сумма положительная");
     }
 
     // ЗАДАНИЕ 3
     public static void printColor() {
-        int value = 75;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
+        out.println("Желтый");
     }
 
     // ЗАДАНИЕ 4
     public static void compareNumbers() {
-        int a = 8;
-        int b = 12;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
+        out.println("a < b");
     }
 
     // ЗАДАНИЕ 5
@@ -135,9 +119,9 @@ public class Homework1 {
     // ЗАДАНИЕ 6
     public static void checkPositiveOrNegative(int number) {
         if (number >= 0) {
-            System.out.println(number + " - положительное");
+            out.println(number + " - положительное");
         } else {
-            System.out.println(number + " - отрицательное");
+            out.println(number + " - отрицательное");
         }
     }
 
@@ -149,7 +133,7 @@ public class Homework1 {
     // ЗАДАНИЕ 8
     public static void printStringMultipleTimes(String str, int count) {
         for (int i = 0; i < count; i++) {
-            System.out.println(str);
+            out.println(str);
         }
     }
 
@@ -199,9 +183,7 @@ public class Homework1 {
     // ЗАДАНИЕ 14
     public static int[] fillArray(int len, int initialValue) {
         int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = initialValue;
-        }
+        Arrays.fill(arr, initialValue);
         return arr;
     }
 }
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-public static void main(String[] args) {
-    chekSumSign();
-    }
-public static void chekSumSign() {
-    System.out.println("Orange\nBanana\nApple");
-    }
-}
