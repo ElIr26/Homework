c452e182c4283368ab372f975c40168e2d494e9f
Ellr26 <eaborovkova@mail.ru>
2026-04-03 13:56:08 +0500

    Выполнено домашнее задание 1-14

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,7 +1,3 @@
-import java.util.Arrays;
-
-import static java.lang.System.*;
-
 public class Homework1 {
     public static void main(String[] args) {
 
@@ -18,8 +14,8 @@ public class Homework1 {
         compareNumbers();
 
         // ЗАДАНИЕ 5
-        out.println(isSumBetween10And20(5, 7));
-        out.println(isSumBetween10And20(1, 2));
+        System.out.println(isSumBetween10And20(5, 7));
+        System.out.println(isSumBetween10And20(1, 2));
 
         // ЗАДАНИЕ 6
         checkPositiveOrNegative(5);
@@ -27,87 +23,109 @@ public class Homework1 {
         checkPositiveOrNegative(0);
 
         // ЗАДАНИЕ 7
-        out.println(isNegative(5));
-        out.println(isNegative(-3));
+        System.out.println(isNegative(5));
+        System.out.println(isNegative(-3));
 
         // ЗАДАНИЕ 8
-        printStringMultipleTimes("Привет, Влад!", 3);
+        printStringMultipleTimes("Hello, QA!", 3);
 
         // ЗАДАНИЕ 9
-        out.println(isLeapYear(2020));
-        out.println(isLeapYear(1900));
+        System.out.println(isLeapYear(2020));
+        System.out.println(isLeapYear(1900));
 
         // ЗАДАНИЕ 10
         int[] binaryArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        out.print("Исходный: ");
-        for (int k : binaryArray) {
-            out.print(k + " ");
+        System.out.print("Исходный: ");
+        for (int i = 0; i < binaryArray.length; i++) {
+            System.out.print(binaryArray[i] + " ");
         }
-        out.println();
+        System.out.println();
         invertBinaryArray(binaryArray);
-        out.print("Результат: ");
-        for (int k : binaryArray) {
-            out.print(k + " ");
+        System.out.print("Результат: ");
+        for (int i = 0; i < binaryArray.length; i++) {
+            System.out.print(binaryArray[i] + " ");
         }
-        out.println();
+        System.out.println();
 
         // ЗАДАНИЕ 11
         int[] hundredArray = createArray1To100();
-        out.print("Первые 10 элементов: ");
+        System.out.print("Первые 10 элементов: ");
         for (int i = 0; i < 10; i++) {
-            out.print(hundredArray[i] + " ");
+            System.out.print(hundredArray[i] + " ");
         }
-        out.println();
+        System.out.println();
 
         // ЗАДАНИЕ 12
         int[] numbers = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        out.print("До: ");
-        for (int number : numbers) {
-            out.print(number + " ");
+        System.out.print("До: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
         }
-        out.println();
+        System.out.println();
         multiplyLessThan6(numbers);
-        out.print("После: ");
-        for (int number : numbers) {
-            out.print(number + " ");
+        System.out.print("После: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
         }
-        out.println();
+        System.out.println();
 
         // ЗАДАНИЕ 13
         int[][] matrix = createDiagonalMatrix(5);
-        for (int[] ints : matrix) {
-            for (int anInt : ints) {
-                out.print(anInt + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
-            out.println();
+            System.out.println();
         }
 
         // ЗАДАНИЕ 14
         int[] filled = fillArray(7, 42);
-        for (int j : filled) {
-            out.print(j + " ");
+        for (int i = 0; i < filled.length; i++) {
+            System.out.print(filled[i] + " ");
         }
-        out.println();
+        System.out.println();
     }
 
     // ЗАДАНИЕ 1
     public static void printThreeWords() {
-        out.println("Orange\nBanana\nApple");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
     // ЗАДАНИЕ 2
     public static void checkSumSign() {
-        out.println("Сумма положительная");
+        int a = 10;
+        int b = -5;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 
     // ЗАДАНИЕ 3
     public static void printColor() {
-        out.println("Желтый");
+        int value = 75;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
     }
 
     // ЗАДАНИЕ 4
     public static void compareNumbers() {
-        out.println("a < b");
+        int a = 8;
+        int b = 12;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 
     // ЗАДАНИЕ 5
@@ -119,9 +137,9 @@ public class Homework1 {
     // ЗАДАНИЕ 6
     public static void checkPositiveOrNegative(int number) {
         if (number >= 0) {
-            out.println(number + " - положительное");
+            System.out.println(number + " - положительное");
         } else {
-            out.println(number + " - отрицательное");
+            System.out.println(number + " - отрицательное");
         }
     }
 
@@ -133,7 +151,7 @@ public class Homework1 {
     // ЗАДАНИЕ 8
     public static void printStringMultipleTimes(String str, int count) {
         for (int i = 0; i < count; i++) {
-            out.println(str);
+            System.out.println(str);
         }
     }
 
@@ -183,7 +201,11 @@ public class Homework1 {
     // ЗАДАНИЕ 14
     public static int[] fillArray(int len, int initialValue) {
         int[] arr = new int[len];
-        Arrays.fill(arr, initialValue);
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
         return arr;
     }
 }
+
+
